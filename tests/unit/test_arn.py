64f48676abf3d1430c0d83f32c5ba7a66bbb7dd2
tobhai <t.haindl@gmx.net>
2020-07-03 13:13:44 +0200

    Fix EC2 addresses

--- tests/unit/test_arn.py
+++ tests/unit/test_arn.py
@@ -360,3 +360,20 @@ class TestARN(unittest.TestCase):
         self.assertEqual(l[0].arn, 'arn:aws:ec2:us-east-1:123456789012:customer-gateway/cgw-030d9af8cdbcdc12f')
         self.assertEqual(l[0].data['CustomerGatewayId'],
                          'cgw-030d9af8cdbcdc12f')
+
+    def test_ec2_address(self):
+        placebo_cfg = {
+            'placebo': placebo,
+            'placebo_dir': self._get_response_path('addresses'),
+            'placebo_mode': 'playback'}
+        arn = scan(
+            'arn:aws:ec2:us-east-1:123456789012:address/*',
+            **placebo_cfg)
+        l = list(arn)
+        self.assertEqual(len(l), 3)
+        self.assertEqual(l[0].arn, 'arn:aws:ec2:us-east-1:123456789012:address/eipalloc-091f2b843804f008c')
+        self.assertEqual(l[0].data['AllocationId'],
+                         'eipalloc-091f2b843804f008c')
+
+        self.assertEqual(l[2].data['Tags'],
+                         [{'Key': 'Name', 'Value': 'some-name'}, {'Key': 'Env', 'Value': 'Prod'}])
