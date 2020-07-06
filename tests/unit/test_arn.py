78e8bc7c1ccd15a81e9a707e8ccdabcd9976ec48
tobhai <t.haindl@gmx.net>
2020-07-06 15:05:26 +0200

    Fix peering connection

--- tests/unit/test_arn.py
+++ tests/unit/test_arn.py
@@ -378,7 +378,6 @@ class TestARN(unittest.TestCase):
         self.assertEqual(l[2].data['Tags'],
                          [{'Key': 'Name', 'Value': 'some-name'}, {'Key': 'Env', 'Value': 'Prod'}])
 
-
     def test_rds_clusters(self):
         placebo_cfg = {
             'placebo': placebo,
@@ -392,4 +391,16 @@ class TestARN(unittest.TestCase):
         self.assertEqual(l[0].arn, 'arn:aws:rds:us-east-1:123456789012:cluster:database-1')
         self.assertEqual(l[0].data['DBClusterIdentifier'],
                          'database-1')
+
+    def test_vpc_peering_connection(self):
+        placebo_cfg = {
+            'placebo': placebo,
+            'placebo_dir': self._get_response_path('peeringconnections'),
+            'placebo_mode': 'playback'}
+        arn = scan(
+            'arn:aws:ec2:us-east-1:123456789012:vpc-peering-connection/*',
+            **placebo_cfg)
+        l = list(arn)
+        self.assertEqual(len(l), 1)
+        self.assertEqual(l[0].arn, 'arn:aws:ec2:us-east-1:123456789012:vpc-peering-connection/pcx-027a582b95db2af78')
 
